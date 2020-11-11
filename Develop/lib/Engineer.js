fef690951fe89523a4e66a80fbe8b67f66e7d4ec
Claire Vita <clairej.vita@gmail.com>
2020-11-11 15:38:52 -0500

    style changes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,19 +1,34 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Employee = require("./Employee.js")
+class Employee{
 
-class Engineer extends Employee {
-    constructor(name, id, email, github) {
-        super(name, id, email)
-        this.github =github
+    constructor(name, id, email) {
+        
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
 
-    getGithub() {
-        return this.github;
+    getName(){
+
+    return this.name;
+
     }
 
-    getRole() {
-        return "Engineer"
+    getId(){
+
+    return this.id;
+
+    }
+
+    getEmail(){
+
+    return this.email;
+
+    }
+
+    getRole(){
+
+    return "Employee";
+
     }
 }
-
-module.exports = Engineer; 
+module.exports = Employee
